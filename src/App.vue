d3f918848b61620b750f08f42c074e104688cbfd
yinchengnuo <yinchengnuo@qq.com>
2021-11-02 09:53:32 +0800

    修改 eslint-plugin-vue 配置 vue3

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
